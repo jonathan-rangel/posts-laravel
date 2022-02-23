ee5d6c43368a0157ed37b40cf90ea49feb04aed0
jonathan <jonathan.rangelrada@gmail.com>
2022-02-22 21:54:33 -0600

    Add posts app

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles.css">
+    <title>My blog</title>
+</head>
+
+<body>
+    <?php foreach($posts as $post) : ?>
+    <article>
+        <a href="/posts/<?= $post->slug; ?>">
+            <h1><?= $post->title; ?></h1>
+        </a>
+
+       <div>
+           <?= $post->excerpt; ?>
+       </div>
+    </article>
+    <?php endforeach; ?>
+</body>
+
+</html>
+
